ecb51c30975ddde9230d419ec9a2d436345aab75
drhpivotal <dherold@pivotal.io>
2017-08-13 18:12:55 -0400

    added bbr banner

--- tasks/bbr-backup-ert/task.sh
+++ tasks/bbr-backup-ert/task.sh
@@ -3,6 +3,14 @@
 . "$(dirname $0)"/../../scripts/export-director-metadata
 
 pushd ert-backup-artifact
+  cat << "EOF"
+ ____                    _               ____  ____  ____
+|  _ \ _   _ _ __  _ __ (_)_ __   __ _  | __ )| __ )|  _ \
+| |_) | | | | '_ \| '_ \| | '_ \ / _` | |  _ \|  _ \| |_) |
+|  _ <| |_| | | | | | | | | | | | (_| | | |_) | |_) |  _ < _ _ _
+|_| \_\\__,_|_| |_|_| |_|_|_| |_|\__, | |____/|____/|_| \_(_|_|_)
+                                 |___/
+EOF
   ../binary/bbr deployment --target "${BOSH_ADDRESS}" \
   --username "${BOSH_CLIENT}" \
   --deployment "${ERT_DEPLOYMENT_NAME}" \
@@ -20,5 +28,5 @@ pushd ert-backup-artifact
 |_____|_| |_|\___|_|   \__, | .__/ \__|_|_| |_|\__, (_|_|_)
                        |___/|_|                |___/
 EOF
-  gpg --batch --yes --quiet --passphrase=$PASSPHRASE -c $BACKUPNAME
+  gpg --batch --yes --quiet --passphrase=$PASSPHRASE -c $BACKUPNAME > /dev/null
 popd
