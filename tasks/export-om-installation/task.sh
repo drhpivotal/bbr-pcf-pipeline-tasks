3c91451485a0172efa3c824c9386b475a40091e5
drhpivotal <dherold@pivotal.io>
2017-08-13 21:02:51 -0400

    use AES256 cipher

--- tasks/export-om-installation/task.sh
+++ tasks/export-om-installation/task.sh
@@ -2,7 +2,15 @@
 
 . $(dirname $0)/../../scripts/om-cmd
 
-BACKUPNAME="installation-$(date '+%m%d%y-%H%M%S').zip"
-om_cmd export-installation --output-file om-installation/$BACKUPNAME
-echo Encrypting $BACKUPNAME
-gpg --yes --quiet --batch --passphrase=$PASSPHRASE -c om-installation/$BACKUPNAME
+BACKUPNAME="installation-$(date '+%m%d%y-%H%M%S')"
+om_cmd export-installation --output-file om-installation/$BACKUPNAME.zip
+cat << "EOF"
+ _____                             _   _
+| ____|_ __   ___ _ __ _   _ _ __ | |_(_)_ __   __ _
+|  _| | '_ \ / __| '__| | | | '_ \| __| | '_ \ / _` |
+| |___| | | | (__| |  | |_| | |_) | |_| | | | | (_| |_ _ _
+|_____|_| |_|\___|_|   \__, | .__/ \__|_|_| |_|\__, (_|_|_)
+                       |___/|_|                |___/
+EOF
+gpg --batch --yes --quiet --cipher-algo AES256 --passphrase=$PASSPHRASE -c $BACKUPNAME > /dev/null
+mv $BACKUPNAME.zip $BACKUPNAME.backup
