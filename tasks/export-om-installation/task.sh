7cc3447568957be48dcdaea683c662c297f394b2
drhpivotal <dherold@pivotal.io>
2017-09-01 16:10:49 -0400

    remove opsmgr export encryption

--- tasks/export-om-installation/task.sh
+++ tasks/export-om-installation/task.sh
@@ -4,13 +4,3 @@
 
 BACKUPNAME="installation-$(date '+%m%d%y-%H%M%S')"
 om_cmd export-installation --output-file om-installation/$BACKUPNAME.zip
-cat << "EOF"
- _____                             _   _
-| ____|_ __   ___ _ __ _   _ _ __ | |_(_)_ __   __ _
-|  _| | '_ \ / __| '__| | | | '_ \| __| | '_ \ / _` |
-| |___| | | | (__| |  | |_| | |_) | |_| | | | | (_| |_ _ _
-|_____|_| |_|\___|_|   \__, | .__/ \__|_|_| |_|\__, (_|_|_)
-                       |___/|_|                |___/
-EOF
-gpg --batch --yes --quiet --cipher-algo AES256 --passphrase=$PASSPHRASE -c om-installation/$BACKUPNAME.zip > /dev/null
-mv om-installation/$BACKUPNAME.zip om-installation/$BACKUPNAME.backup
